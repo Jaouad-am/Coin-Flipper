f6ffb80d1344029525a82ff77a95cbd6f079fa81
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2020-02-27 04:19:05 +0100

    CoinFlipper Component part3

--- src/CoinFlipper.js
+++ src/CoinFlipper.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import { choice } from "./Helpers";
 import Coin from "./Coin";
 
 class CoinFlipper extends Component {
-  static defaultProps = [
-    { side: "heads", imgSrc: "https://tinyurl.com/react-coin-heads-jpg" },
-    { side: "tails", imgSrc: "https://tinyurl.com/react-coin-tails-jpg" },
-  ];
+  static defaultProps = {
+    coins: [
+      { side: "heads", imgSrc: "https://tinyurl.com/react-coin-heads-jpg" },
+      { side: "tails", imgSrc: "https://tinyurl.com/react-coin-tails-jpg" },
+    ],
+  };
   constructor(props) {
     super(props);
     this.state = {
@@ -17,19 +20,28 @@ class CoinFlipper extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
   flip() {
-    console.log("clicked");
+    const randCoin = choice(this.props.coins);
+    //console.log(randCoin);
+    this.setState((st) => {
+      return {
+        coin: randCoin,
+        nFlips: st.nFlips + 1,
+        nHeads: st.nHeads + (randCoin.side === "heads" ? 1 : 0),
+        nTails: st.nTails + (randCoin.side === "tails" ? 1 : 0),
+      };
+    });
   }
-  handleClick() {
+  handleClick(e) {
     this.flip();
   }
   render() {
     return (
       <div className="CoinFlipper">
         <h1>Coin Flip</h1>
-        <Coin />
+        <Coin data={this.state.coin} />
         <button onClick={this.handleClick}>Flip</button>
         <p>
-          you did {this.state.nFlips} Flips, you got {this.state.nHeads} Heads &{" "}
+          Out Of {this.state.nFlips} Flips, you got {this.state.nHeads} Heads &{" "}
           {this.state.nTails} Tails
         </p>
       </div>
